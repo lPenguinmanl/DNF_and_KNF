a607eeee49f202cd09f164d07616c20993535cc0
Penguinman <valeriysmirnovv190702@gmail.com>
2020-05-16 16:41:46 +0300

    Understood  input from file.  Make a check that amount of elements in file(FUNCTIONS.txt) be power of two. Add try catch.

--- DNF_CNF/DNF_CNF.cpp
+++ DNF_CNF/DNF_CNF.cpp
@@ -1,11 +1,28 @@
 ﻿#include<iostream>
 #include<fstream>
+#include"DNF_CNF.h"
 using namespace std;
 int main()
 {
 	ifstream File("FUNCTIONS.txt");
 	int i = 0, func_res;
-	cout << File.gcount() << endl;
+	//получаю количество ел будевой функции
+	while (File>>func_res)
+	{
+		i++;
+	}
 	File.close();
+	try
+	{
+		if (!Is_log2(i))
+		{
+			throw -1;
+		}
+	}
+	catch (int i)
+	{
+		cout << "Error data in file" << endl;
+		exit(0);
+	}
 	return 0;
 }
