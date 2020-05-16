b6fe0f4c6a4a435b326a0df8ac20e65002583f7d
Penguinman <valeriysmirnovv190702@gmail.com>
2020-05-16 17:24:57 +0300

    Add two cycles for future CNF and DNF

--- DNF_CNF/DNF_CNF.cpp
+++ DNF_CNF/DNF_CNF.cpp
@@ -2,32 +2,56 @@
 #include<fstream>
 #include<math.h>
 using namespace std;
+bool Is_exp_of_2(int n);
 int main()
 {
 	ifstream File("FUNCTIONS.txt");
-	int i = 0, func_res;
+	int amount = 0, func_res;
 	//получаю количество ел будевой функции
 	while (File>>func_res)
 	{
-		i++;
+		amount++;
 	}
 	File.close();
 	try
 	{
-		if (!Is_log2(i))
+		if (!Is_exp_of_2(amount))
 		{
 			throw -1;
 		}
 	}
 	catch (int i)
 	{
-		cout << "Error data in file" << endl;
+		cout << "Error "<<i<<" :data in file" << endl;
 		exit(0);
 	}
+	//считываем булевую функцию и записываем ее в нассив
+	int* Function = new int [amount];
+	ifstream file("FUNCTIONS.txt");
+	for (int i = 0; i < amount; i++)
+	{
+		file>>Function[i];
+	}
+	//посторйка КНФ
+	for (int i = 0; i < amount; i++)
+	{
+		if (Function[i]==0)
+		{
+			
+		}
+	}
+	//посторйка ДНФ
+	for (int i = 0; i < amount; i++)
+	{
+		if (Function[i] == 1)
+		{
 
+		}
+	}
+	file.close();
 	return 0;
 }
-bool Is_log2(int n)
+bool Is_exp_of_2(int n)
 {
 	if (n == 0) {
 		return false;
