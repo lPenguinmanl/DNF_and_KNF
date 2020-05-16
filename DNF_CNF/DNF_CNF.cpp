91d8800c4c9e0ad09245f5b3ce4ca930848e4f91
Penguinman <valeriysmirnovv190702@gmail.com>
2020-05-16 15:03:41 +0300

    Added file for reading with program.

--- DNF_CNF/DNF_CNF.cpp
+++ DNF_CNF/DNF_CNF.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ifstream File("FUNCTIONS.txt");
+	int i = 0, func_res;
+	cout << File.gcount() << endl;
+	File.close();
+	return 0;
+}
