a66b92f017c28b05705e5e0592dab225bd9f5db9
Penguinman <valeriysmirnovv190702@gmail.com>
2020-05-18 15:48:41 +0300

    I finished this project.  I had written two function CNf and KNF. U will see one broblem: all comments in Russian. But i this this project nobody see ;)

--- DNF_CNF/DNF_CNF.cpp
+++ DNF_CNF/DNF_CNF.cpp
@@ -2,13 +2,15 @@
 #include<fstream>
 #include<math.h>
 #include<list>
+#include<algorithm>
 using namespace std;
 bool Is_exp_of_2(int n);
 void DNF(int I, int x);
+void CNF(int I, int x);
 int main()
 {
 	ifstream File("FUNCTIONS.txt");
-	int amount = 0, func_res;
+	int amount = 0, func_res, amount_of_one = 0, amount_of_zero = 0, current_zero = 0, current_one = 0;
 	//получаю количество ел будевой функции
 	while (File>>func_res)
 	{
@@ -34,25 +36,48 @@ int main()
 	{
 		file>>Function[i];
 	}
-	/*
+	//подсчет 1 и 0
+	for (int i = 0; i < amount; i++)
+	{
+		if (Function[i] == 0)
+		{
+			amount_of_zero++;
+		}
+		else
+		{
+			amount_of_one++;
+		}
+	}
 	//посторйка КНФ
+	cout << "CNF:" << endl;
 	for (int i = 0; i < amount; i++)
 	{
 		if (Function[i]==0)
 		{
-			
+			CNF(i, log2(amount));
+			current_zero++;
+			if (current_zero!=amount_of_zero)
+			{
+				cout << "&";
+			}
 		}
 	}
+	cout << endl;
+	cout << "DNF:" << endl;
 	//посторйка ДНФ
 	for (int i = 0; i < amount; i++)
 	{
 		if (Function[i] == 1)
 		{
-
+			DNF(i, log2(amount));
+			current_one++;
+			if (current_one != amount_of_one)
+			{
+				cout << "|";
+			}
 		}
 	}
-	*/
-	DNF(3,4);
+	
 	file.close();
 	return 0;
 }
@@ -65,24 +90,109 @@ bool Is_exp_of_2(int n)
 	for (; n % 2 == 0; n /= 2);
 	return (n == 1);
 }
-void DNF(int I, int x)
+void DNF(int I, int size)
 {
 	//массив переменных которые будем использовать для построение ДНФ
-	char* variables = new char[x];
-	int* Bin = new int[x];
-	int now =0 ;
-	for (int i = 0; i < x; i++)
+	char* variables = new char[size];
+	int* Bin = new int[size];
+	int now = 0 ;
+	for (int i = 0; i < size; i++)
 	{
 		variables[i] = 97 + i;
 	}
-	while (I!=0)
-	{ 
+	while (I != 0)
+	{
 		Bin[now] = I % 2;
 		I /= 2;
-		now++;
+		now++;// позиция последнего добавленого элемента
 	}
-	for (int i = 0; i < x; i++)
+	if (now != size - 1)
 	{
-		cout << Bin[i];
+		for (int i = 0; i < now + 1; i++)
+		{
+			swap(Bin[now - i], Bin[size - i]);
+		}
 	}
+	int k = 0;
+	cout << "(";
+	for (int i = size - 1; i >= 0; i--)
+	{
+		if (i == 0)
+		{
+			if (Bin[i] != 1)
+			{
+				cout << "!" << variables[size - 1]<<")";
+			}
+			else
+			{
+				cout << variables[size - 1] << ")";
+			}
+		}
+		else
+		{
+			if (Bin[i] != 1)
+			{
+				cout << "!" << variables[k] << "&";
+			}
+			else
+			{
+				cout << variables[k] << "&";
+			}
+		}
+		k++;
+	}
+		
+}
+void CNF(int I, int size)
+{
+	//массив переменных которые будем использовать для построение ДНФ
+	char* variables = new char[size];
+	int* Bin = new int[size];
+	int now = 0;
+	for (int i = 0; i < size; i++)
+	{
+		variables[i] = 97 + i;
+	}
+	while (I != 0)
+	{
+		Bin[now] = I % 2;
+		I /= 2;
+		now++;// позиция последнего добавленого элемента
+	}
+	if (now != size - 1)
+	{
+		for (int i = 0; i < now + 1; i++)
+		{
+			swap(Bin[now - i], Bin[size - i]);
+		}
+	}
+	int k = 0;
+	cout << "(";
+	for (int i = size - 1; i >= 0; i--)
+	{
+		if (i == 0)
+		{
+			if (Bin[i] != 1)
+			{
+				cout << variables[size - 1]<<")";
+			}
+			else
+			{
+				cout <<"!"<< variables[size - 1]<<")";
+			}
+		}
+		else
+		{
+			if (Bin[i] != 1)
+			{
+				cout << variables[k] << "|";
+			}
+			else
+			{
+				cout << "!" << variables[k] << "|";
+			}
+		}
+		k++;
+	}
+
 }
